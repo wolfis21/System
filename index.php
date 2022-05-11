048c2c21a31cc5c7394d52cadcaf6da8c409e112
Isaac <isaacsaado69@gmail.com>
2022-05-11 00:11:01 -0400

    ajaaa

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     <div class="well well-sm text-right">
         <a class="btn btn-primary" href="home_empleado.php">Seccion Empleados</a>
         <a class="btn btn-primary" href="home_clientes.php">Seccion Clientes</a>
-        <a class="btn btn-primary" href="home_equipos.php">Seccion Equipos</a>
+        <a class="btn btn-primary" href="home_equipo.php">Seccion Equipos</a>
         <a href="view/login/login.php">Iniciar Seccion</a>
     </div>
 
