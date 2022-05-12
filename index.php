e3c26774b6ae0dfc7ba91a6c88b3bae35a81742e
Isaac <isaacsaado69@gmail.com>
2022-05-11 20:50:28 -0400

    corerccion

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <a class="btn btn-primary" href="home_empleado.php">Seccion Empleados</a>
         <a class="btn btn-primary" href="home_cliente.php">Seccion Clientes</a>
         <a class="btn btn-primary" href="home_equipo.php">Seccion Equipos</a>
-        <a href="view/login/login.php">Iniciar Seccion</a>
+        <a href="view/login/login.php">Iniciar Sesion</a>
     </div>
 
     <?php require_once 'view/footer.php'; ?>
